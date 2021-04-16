0ee0bb33e80f141cbee492165c58a69e69ea0277
Gabriel <gaplogan@gmail.com>
2021-04-16 17:26:49 -0300

    2021.04.16.17.26

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace cs_1._25_CalculaHipotenusa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cat1, cat2, hipo;
+
+            Console.Clear();
+
+            Console.Write("Digite o valor do primeiro cateto: ");
+            cat1 = int.Parse(Console.ReadLine());
+            cat1 = Math.Abs(cat1);
+
+            Console.Write("Digite o valor do segundo cateto: ");
+            cat2 = int.Parse(Console.ReadLine());
+            cat2 = Math.Abs(cat2);
+
+            hipo = (int)Math.Sqrt(Math.Pow(cat1, 2) + Math.Pow(cat2, 2));
+
+            Console.WriteLine($"Valor da hipotenusa: {hipo}");
+        }
+    }
+}
